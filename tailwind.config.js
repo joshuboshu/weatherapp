654fc245a9c91e029991866567d42790bb7e3e88
joshuboshu <joshuboshu@gmail.com>
2024-08-01 12:14:39 -0400

    update fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
